ac9b8a2eedc79db5402ea126ae60ea937c040806
ChandanKrv <chandan.nit.cse@gmail.com>
2024-08-04 20:19:11 +0530

    initial commit

---
+++ MyClassera-Backend/src/main/java/com/chandankrv/myclassera/MyClasseraApplication.java
@@ -0,0 +1,13 @@
+package com.chandankrv.myclassera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClasseraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyClasseraApplication.class, args);
+	}
+
+}
---
+++ MyClassera-Backend/src/test/java/com/chandankrv/myclassera/MyClasseraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandankrv.myclassera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyClasseraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
