cdcbe63ce69c4977a0be438b3ba97f251c2960d5
ChandanKrv <chandan.nit.cse@gmail.com>
2024-08-04 22:11:38 +0530

    initial commit

---
+++ MyClassera-Backend/src/main/java/com/chandankrv/myclassera/model/Subject.java
@@ -0,0 +1,9 @@
+package com.chandankrv.myclassera.model;
+
+/**
+ * Created by Chandan on 04 August, 2024.
+ * --------------------------------------
+ * Q. Problem Statement :
+ */
+public class Subject {
+}
